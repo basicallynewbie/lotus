889a7715a377073d9eb811bb724471cf8fb7986c
basicallynewbie <107107611+basicallynewbie@users.noreply.github.com>
2024-10-04 22:37:11 +0800

    Update lotus.py

--- lotus.py
+++ lotus.py
@@ -14,7 +14,9 @@ class Lotus():
             pass
 
         def getInfo(self, filename: str, metadata: dict) -> None:
-            self.filename = Path(filename).name
+            self.filename = filename
+            if bool(re.search(r'\\|/', self.filename, re.IGNORECASE)):
+                exit('filename is not a path')
             self.index = metadata['index']
             self.length = metadata['length']
             self.offset = metadata['offset']
@@ -22,62 +24,57 @@ class Lotus():
             self.series = metadata['series']
             self.target_folder = Path(metadata['target_folder'])
             self.subfolder = metadata['subfolder']
-            self.name_list = [metadata['title_prefix'], metadata['title_rest']]
+            self.name_list = [metadata['title_prefix'], metadata['title_postfix']]
 
         def getEpisode(self) -> str:
-            self.episode = ''
-            try:
-                episode = re.findall(r'\d+', self.filename)
-                extra_info = r'pv|teaser|trailer|scene|clip|interview|extra|deleted'
-                if bool(re.search(extra_info, self.filename, re.IGNORECASE)):
-                    self.episode = ''
-                elif episode == re.findall(r'\d+', self.template):
-                    self.episode = ''
-                else:
-                    self.episode = episode[int(self.index)]
-            except:
+            episode = re.findall(r'\d+', self.filename)
+            if episode == []:
                 self.episode = ''
-            return self.episode
-        
+                return self.episode
+            extra_info = r'pv|teaser|trailer|scene|clip|interview|extra|deleted'
+            if bool(re.search(extra_info, self.filename, re.IGNORECASE)):
+                self.episode = ''
+                return self.episode
+            elif episode == re.findall(r'\d+', self.template):
+                self.episode = ''
+                return self.episode
+            else:
+                self.episode = episode[int(self.index)]
+                return self.episode
+
         def getExtension(self) -> str:
             if not bool(re.findall(r'\.', self.filename)):
                 self.extension = ''
+                return self.extension
             else:
                 self.extension = self.filename.split('.')[-1]
-            return self.extension
-        
-        def getExtra(self) -> str:
-            extra = ''
-            if bool(self.template):
-                extra = self.template
-                extra = [x for x in ndiff(extra, self.filename) if x[0] != ' ']
-                extra = ''.join([x[2] for x in extra])
+                return self.extension
 
-                # get rid of self.episode
-                try:
-                    trim = re.match(r'\d', extra)
-                    if trim != None:
-                        extra = re.sub(self.episode, r'', extra, 1)
-                except:
-                    pass
-                try:
-                    if bool(self.extension):
-                        extra = re.sub(r'.' + self.extension, r'', extra, 1)
-                except:
-                    pass
-            self.extra = extra
+        def getExtra(self) -> str:
+            extra = self.template
+            extra = [x for x in ndiff(extra, self.filename) if x[0] != ' ']
+            self.extra = ''.join([x[2] for x in extra])
+            # get rid of self.episode
+            try:
+                if re.match(r'\d', self.extra) != None:
+                    self.extra = re.sub(self.episode, r'', self.extra, 1)
+            except:
+                pass
+            if bool(self.extension):
+                self.extra = re.sub(r'.' + self.extension, r'', self.extra, 1)
             return self.extra
-        
+
         def getTargetName(self) -> str:
             if self.series:
                 self.getEpisode()
                 if bool(self.episode):
                     if self.offset:
-                        self.episode = str(int(self.episode) - self.offset)
-                    self.name_list.insert(1, '.E' + self.episode.zfill(self.length))
+                        self.true_episode = str(int(self.episode) - self.offset)
+                    self.name_list.insert(1, '.E' + self.true_episode.zfill(self.length))
             self.getExtension()
-            if bool(self.getExtra()):
-                self.name_list.append('.' + self.extra)
+            if bool(self.template):
+                if bool(self.getExtra()):
+                    self.name_list.append('.' + self.extra)
             if bool(self.extension):
                 self.name_list.append('.' + self.extension)
 
@@ -85,23 +82,23 @@ class Lotus():
             for i in self.name_list:
                 self.target_name += i
             return self.target_name
-        
+
         def getTargetFolder(self):
             self.ture_target_folder = self.target_folder
             try:
                 if self.subfolder and bool(self.episode):
                     self.ture_target_folder = self.ture_target_folder.joinpath(
-                            self.episode.zfill(self.length)
-                        )
+                                                    self.true_episode.zfill(self.length)
+                                                    )
             except:
                 pass
             return self.ture_target_folder
-        
+
         def getTargetPath(self):
             self.target_path = self.ture_target_folder.joinpath(self.target_name)
             return self.target_path
 
-    def getUserInput(self, path: str, jsonfile: str, recursive: bool = False) -> None:
+    def setUserInput(self, path: str, jsonfile: str, recursive: bool = False) -> None:
         self.path = Path(path)
         self.jsonfile = Path(jsonfile)
         self.recursive = recursive
@@ -191,65 +188,53 @@ class Lotus():
         self.title_prefix = self.title_prefix[1:]
         tempdict.update({'title_prefix': self.title_prefix})
 
-        self.title_rest = ''
+        self.title_postfix = ''
         for j in self.metadata.values():
             if bool(j):
-                self.title_rest += f'.{j}'
-        tempdict.update({'title_rest': self.title_rest})
+                self.title_postfix += f'.{j}'
+        tempdict.update({'title_postfix': self.title_postfix})
 
         self.metadata.clear()
         self.metadata.update(tempdict)
         return self.metadata
-    
-    def autolink(self) -> None:
-        def testLinkAction(origin_path: str) -> None:
-            if not self.fileProcess.target_path.is_file():
-                print(f'{origin_path} <==> {self.fileProcess.target_path} \n')
-            else:
-                print(f'"{self.fileProcess.target_path}" already exist')
-        
+
+    # can't softlink in windows due to PermissionError
+    def linkAction(self, origin_path: str) -> None:
         def createLinkFolder() -> None:
             if not self.fileProcess.ture_target_folder.is_dir():
                 try:
                     self.fileProcess.ture_target_folder.mkdir(parents=True)
                 except:
                     exit(f'no permission to create "{self.fileProcess.ture_target_folder}"')
-        # can't softlink in windows due to PermissionError
-        def trueLinkAction(origin_path: str) -> None:
-            def action(input_path):
-                if self.link_option == 'hardlink':
-                    self.fileProcess.target_path.hardlink_to(input_path)
-                elif self.link_option == 'softlink':
-                    self.fileProcess.target_path.symlink_to(input_path, target_is_directory=False)
-                else:
-                    pass
-
-            try:
+        def action(input_path):
+            if self.link_option == 'hardlink':
+                self.fileProcess.target_path.hardlink_to(input_path)
+            elif self.link_option == 'softlink':
+                self.fileProcess.target_path.symlink_to(input_path, target_is_directory=False)
+            else:
+                pass
+        try:
+            if not self.link_option == 'test':
                 createLinkFolder()
-                if not self.fileProcess.target_path.is_file():
-                    action(origin_path)
-                    print(f'{origin_path} <==> {self.fileProcess.target_path}')
-            except:
-                exit(f'no permission to create "{self.fileProcess.target_path}"')
+            if not self.fileProcess.target_path.is_file():
+                action(origin_path)
+                print(f'{origin_path} <==> {self.fileProcess.target_path}')
+            else:
+                print(f'"{self.fileProcess.target_path}" already exist')
+        except:
+            exit(f'no permission to create "{self.fileProcess.target_path}"')
 
-        def fileAction(origin_path_file):
-            self.fileProcess.getInfo(origin_path_file, self.metadata)
+    def autolink(self) -> None:
+        def fileAction(origin_file_path):
+            self.fileProcess.getInfo(origin_file_path.name, self.metadata)
             self.fileProcess.getTargetName()
             self.fileProcess.getTargetFolder()
             self.fileProcess.getTargetPath()
+            self.linkAction(origin_file_path)
 
-            if self.link_option == 'test':
-                testLinkAction(origin_path_file)
-            elif self.link_option == 'hardlink' or 'softlink':
-                trueLinkAction(origin_path_file)
-            else:
-                exit('link_option only has 3 choices: test, hardlink, softlink')
-
-        zone = None
+        zone = self.path.iterdir()
         if self.recursive:
             zone = self.path.rglob('*')
-        else:
-            zone = self.path.iterdir()
 
         if self.path.is_dir():
             for i in zone:
@@ -262,12 +247,14 @@ class Lotus():
 
     def execute(self, link_option: str) -> None:
         self.link_option = link_option
+        if link_option.casefold() not in ['hardlink', 'softlink', 'test']:
+            exit('link_option only has 3 choices: test, hardlink, softlink')
         self.checkUserInput()
         self.importMetadata()
         self.checkMetadata()
         self.processMetadata()
         self.autolink()
-    
+
     def cli(self) -> None:
         parser = ArgumentParser(
             prog='lotus',
@@ -278,9 +265,9 @@ class Lotus():
 
         subparsers.add_parser('test', help= 'print outcome without action')
         subparsers.add_parser('hardlink', 
-                            help= 'Make file/s in path a hard link pointing to target')
+                                help= 'Make file/s in path a hard link pointing to target')
         subparsers.add_parser('softlink', 
-                            help= 'Make file/s in path a symbolic link pointing to target')
+                                help= 'Make file/s in path a symbolic link pointing to target')
 
         parser.add_argument('path', type= str, help= 'The source path of your file or folder')
         parser.add_argument('jsonfile', type= str, help= 'The source path of your jsonfile')
@@ -289,7 +276,7 @@ class Lotus():
 
         args = parser.parse_args()
 
-        self.getUserInput(args.path, args.jsonfile, args.recursive)
+        self.setUserInput(args.path, args.jsonfile, args.recursive)
         self.execute(args.option)
 
 if __name__ == '__main__':
